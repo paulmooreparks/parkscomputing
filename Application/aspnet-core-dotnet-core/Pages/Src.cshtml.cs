32a8cfe922669f8a189927c749930227989e3cad
Paul Parks <paul@smartsam.com>
2022-12-28 08:58:43 +0800

    Added page and view for retrieving and formatting code snippets; tweaked to enable Accelerated C++ Solutions.

---
+++ Application/aspnet-core-dotnet-core/Pages/Src.cshtml.cs
@@ -0,0 +1,49 @@
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Globalization;
+using System;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace aspnet_core_dotnet_core.Pages {
+    public class SrcModel : PageModel {
+        public string PageContent { get; set; }
+
+        private IHostEnvironment Environment { get; set; }
+
+        public SrcModel(IHostEnvironment environment) {
+            Environment = environment;
+        }
+
+        public async Task<IActionResult> OnGetAsync() {
+            ViewData.Add("language", "plaintext");
+            object slugObject = HttpContext.Request.RouteValues["slug"];
+            string slug = string.Empty;
+
+            if (slugObject is not null) {
+                slug = slugObject.ToString().Trim('/');
+            }
+
+            try {
+                var path = $"{Environment.ContentRootPath}/wwwroot/{slug}";
+                PageContent = await System.IO.File.ReadAllTextAsync(path);
+                var encoder = HtmlEncoder.Default;
+                PageContent = encoder.Encode(PageContent);
+
+                if (Request.Query.ContainsKey("language")) {
+                    ViewData["language"] = $"language-{Request.Query["language"]}";
+                }
+            }
+            catch (Exception) {
+                throw;
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Application/aspnet-core-dotnet-core/wwwroot/content/accelerated-cpp-solutions/src/exercise-0-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout
+        << "#include <iostream>\n"
+        << "\n"
+        << "int main()\n"
+        << "{\n"
+        << "   std::cout << \"Hello, world!\" << std::endl;\n"
+        << "   return 0;\n"
+        << "}\n";
+
+    return 0;
+}
